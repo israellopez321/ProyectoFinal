daa99faad92f7cc098fdcdb7a939dcbf99dd3058
darkx <israel.lopez@iesnervion.com>
2026-05-14 21:45:34 +0200

    Creacion clase turnos

---
+++ ProyectoFinal/src/mechanics/TurnManager.java
@@ -0,0 +1,11 @@
+package mechanics;
+
+import java.util.ArrayList;
+import model.enemies.Enemy;
+
+public class TurnManager {
+
+	private ArrayList<Character> characters;
+	private ArrayList<Enemy> enemies;
+	
+}
