ea1c28d844b4847475633c408698fef74af19e17
darkx <israel.lopez@iesnervion.com>
2026-05-21 22:09:28 +0200

    Creacion de clase Items

---
+++ ProyectoFinal/src/model/items/HealItem.java
@@ -0,0 +1,9 @@
+package model.items;
+
+public class HealItem {
+
+	
+	
+	
+	
+}
--- ProyectoFinal/src/model/items/Item.java
+++ ProyectoFinal/src/model/items/Item.java
@@ -1,16 +1,36 @@
 package model.items;
 
-public class Item {
+public abstract class Item {
 	
 	protected String id;
 	protected String name;
 	protected String description;
+	protected String type; // e.g., "heal", "damage", "buff", etc.
 	
-	public Item(String id, String name, String description) {
+	public Item(String id, String name, String description, String type) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
+		this.type = type;
 	}
+	
+	// Getters
+	public String getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * Abstract method that defines the behavior when using an item.
+	 */
+	public abstract void use();
 	
 	
 
